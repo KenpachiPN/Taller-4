0d11055e7c2e7d918747d57ad2d9bdbcddf2dd51
KenpachiPN <sprada71@gmail.com>
2022-10-04 00:59:43 -0500

    Ejercicio #9

--- main.js
+++ main.js
@@ -1,24 +1,25 @@
 
-let canNum = Number(prompt("Cuantos numeros desea ingresar"));
-let suma = 0;
-let resta = 0;
-let multi = 1;
-let divi = 1;
-let nums;
-for(let i1 = 1; i1 <= 5; i1++) {
-    for(let i = 1; i <= canNum; i++) {
-    nums = Number(prompt("Ingrese el # " + i )); 
-    suma += nums;
-    resta -= nums;
-    multi *= nums;
-    divi /= nums;
+let iva = 0.16;
+let total;
+let precioIva;
+let precioUnid;
+let descuento;
+for(let i = 1; i <= 2; i++) {
+    let precio = Number(prompt("Ingrese el precio del articulo"));
+    let unidades = Number(prompt("Ingrese las unidades que comprará del articulo"));
+    alert("El precio del articulo es de: $" + precio + " pesos, luego de esto se le aplicara un iva del 16% y se le sumará las " + unidades + " unidades que usted pidió de ese producto");
+    precioUnid = precio * unidades;
+    precioIva = precioUnid * iva;
+    total = precioUnid + iva;
+    if (total > 500) {
+        descuento = total * 0.15;
+        descuento -= total;
+        alert("Debido a que su compra excedió los $500 pesos, recibió un descuento del 15%, el total es de: $" + descuento + " pesos")
+    } 
+    else {
+    alert("El valor a pagar es de: $" + total + " pesos");
 
-}
-alert("La suma de los números es: " + suma);
-alert("La resta de los números es: " + resta);
-alert("La multiplicación de los números es: " + multi);
-alert("La división de los números es: " + divi);
-
+    }
 }
 
 
