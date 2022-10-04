65f2ad5960a37544b94852d6569d9a05efac5cc0
KenpachiPN <sprada71@gmail.com>
2022-10-03 22:36:04 -0500

    Ejercicio #3

--- main.js
+++ main.js
@@ -1,10 +1,25 @@
 
-    alert("Generar la serie: 1, 5, 3, 7, 5, 9, 7, ..., 23");
-let x = 0;
-let y = 1;
-for(let i = 1; i <= 13; i++ ){
-    x = y + 4;
-    y = x-2;
-    alert("La serie es: " + y)
-};
-
+let estudiantes = Number(prompt("Ingrese la cantidad de estudiantes que calificarán la comida"));
+let suma = 0;
+let promedio;
+for(let i = 1; i <= estudiantes; i++){
+    let nom = prompt("Ingrese su nombre");
+    let calif = Number(prompt(nom + ", ingrese su calificación; 1 o 10"));
+    if(calif === 1 ) {
+        alert("La calificación de " + nom + " es horrible");
+        suma += calif;
+    }
+    else if (calif === 10) {
+        alert("La calificación de " + nom + " es excelente");
+        suma += calif;
+    }
+    if (calif === 0 ) {
+        break;
+    }
+    promedio = suma / estudiantes;
+}
+    
+    alert("El promedio de calificación de los estudiantes sobre la comida es de: " + promedio );
+    alert("Fueron encuestados " + estudiantes + " estudiantes");
+    alert("La nota más alta fue de:  10 ");
+    alert("La nota más baja fue de:   1 ");
