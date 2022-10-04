55e79b3090147e7595c02e8ff680829aff3b863f
KenpachiPN <sprada71@gmail.com>
2022-10-04 01:05:51 -0500

    Ejercicio #10

--- main.js
+++ main.js
@@ -1,26 +1,14 @@
 
-let iva = 0.16;
-let total;
-let precioIva;
-let precioUnid;
-let descuento;
-for(let i = 1; i <= 2; i++) {
-    let precio = Number(prompt("Ingrese el precio del articulo"));
-    let unidades = Number(prompt("Ingrese las unidades que comprará del articulo"));
-    alert("El precio del articulo es de: $" + precio + " pesos, luego de esto se le aplicara un iva del 16% y se le sumará las " + unidades + " unidades que usted pidió de ese producto");
-    precioUnid = precio * unidades;
-    precioIva = precioUnid * iva;
-    total = precioUnid + iva;
-    if (total > 500) {
-        descuento = total * 0.15;
-        descuento -= total;
-        alert("Debido a que su compra excedió los $500 pesos, recibió un descuento del 15%, el total es de: $" + descuento + " pesos")
-    } 
-    else {
-    alert("El valor a pagar es de: $" + total + " pesos");
-
+let x = 1;
+let suma = 0;
+let y = Number(prompt("Ingrese la cantidad de números que desea hacer"));
+for(let i = 1; i <= y+1; i++) {
+    let z = i**x
+    suma = z + suma
+        alert("El resultado de elevar " + y + " y " + x + " dió " + z);
+        alert("Al sumar los otros resultados da: " + suma);
     }
-}
+
 
 
 
