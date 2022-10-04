e4a16b9fb443b3d94a5a4cb0a4c17b927d79a335
KenpachiPN <sprada71@gmail.com>
2022-10-04 00:07:58 -0500

    Ejercicio #6

--- main.js
+++ main.js
@@ -1,28 +1,21 @@
 
-let ventasdia = Number(prompt("Ingrese el número de ventas que hizo al día"));
+let trabajadores = 5;
 
-let monto1 = 0;
-let monto2 = 0;
-let monto3 = 0;
-let total = 0;
-
-for(let i = 1; i <= ventasdia; i++) {
-    let montoventa = Number(prompt("Ingrese el monto de la venta # " + i));
-    if (montoventa > 1000) {
-        monto1 += montoventa;
-        alert("El monto vendido en la categoría > mil es de: $" + monto1 + " pesos");
+for(let i = 1; i <= trabajadores; i++) {
+    let tiemposervicio = Number(prompt("Hola trabajador # " + i + ", ingrese el tiempo que lleva trabajando en esta empresa"));
+    if (tiemposervicio < 5) {
+        alert("Gracias por su responder de manera sincera, debido a que usted lleva con nostros " + tiemposervicio + " años, recibirá un aumento de 100 pesos en su salario");
     }
-    else if (montoventa <= 1000 && montoventa > 500) {
-        monto2 += montoventa;
-        alert("El monto vendido en la categoría > a 500 y menor a mil es de: $" + monto2 + " pesos");
+    else if (tiemposervicio >= 5 && tiemposervicio <= 9) {
+        alert("Gracias por su responder de manera sincera, debido a que usted lleva con nostros " + tiemposervicio + " años, recibirá un aumento de 250 pesos en su salario");
     }
-    else if (montoventa <= 500) {
-        monto3 += montoventa;
-        alert("El monto vendido en la categoría < a 500: $" + monto3 + " pesos");
+    else if (tiemposervicio >= 10 && tiemposervicio <= 19) {
+        alert("Gracias por su responder de manera sincera, debido a que usted lleva con nostros " + tiemposervicio + " años, recibirá un aumento de 400 pesos en su salario");
     }
-    total += montoventa;
+    else {
+        alert("Gracias por su responder de manera sincera, debido a que usted lleva con nostros " + tiemposervicio + " años, recibirá un aumento de 550 pesos en su salario");
+    }
 }
-    alert("El total de las ventas al día es de: $" + total + " pesos ");
 
 
 
